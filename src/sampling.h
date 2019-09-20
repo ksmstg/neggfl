002315e3747b565bea5c4f7b7774ef6a2dd5f21b
kaizin <kshimamr@gmail.com>
2019-09-21 01:51:06 +0900

    Bug Fix

--- src/sampling.h
+++ src/sampling.h
@@ -7,4 +7,8 @@
 double rand_gamma( double theta, double kappa );
 double rand_invgauss(double mu, double lambda);
 
+void init_genrand(unsigned long s);
+unsigned long genrand_int32(void);
+double genrand_real3(void);
+
 #endif
